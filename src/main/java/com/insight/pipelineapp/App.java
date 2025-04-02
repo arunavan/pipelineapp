cdf597476a49e61171658a9fbb3b8cfe7dc96b6d
arunajava567 <arunajava567@gmail.com>
2025-04-02 15:44:41 +0530

    first commit

---
+++ src/main/java/com/insight/pipelineapp/App.java
@@ -0,0 +1,13 @@
+package com.insight.pipelineapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
